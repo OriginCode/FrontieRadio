ec458ff3b4bec9db18dbeda1ceea8c0287516464
Kaiyang Wu <self@origincode.me>
2024-12-05 04:53:14 -0800

    feat(server): add heartbeat processing
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- server.rkt
+++ server.rkt
@@ -5,17 +5,29 @@
          net/rfc6455
          json)
 
+(ws-idle-timeout 6000)
+
 (define (connection-handler c state)
   (displayln "connection received")
-  (let loop ([previnfo #f])
-    (define mpd-conn (mpd-connect))
-    (define currinfo
-      (hash 'current (mpd-currentsong mpd-conn) 'next (mpd-nextsong mpd-conn)))
-    (mpd-close-connection mpd-conn)
-    (when (not (equal? previnfo currinfo))
-        (ws-send! c (jsexpr->bytes currinfo)))
-    (sleep 5)
-    (loop currinfo)))
+  (thread (λ ()
+            (let loop ([previnfo #f])
+              (define mpd-conn (mpd-connect))
+              (define currinfo
+                (hash 'current (mpd-currentsong mpd-conn) 'next (mpd-nextsong mpd-conn)))
+              (mpd-close-connection mpd-conn)
+              (when (not (equal? previnfo currinfo))
+                (ws-send! c (jsexpr->bytes currinfo)))
+              (sleep 1)
+              (loop currinfo))))
+  (let loop ()
+    (match (ws-recv c #:payload-type 'text)
+      [(? eof-object?) (void)]
+      ["ping"
+       (displayln "recv ping")
+       (ws-send! c "pong")
+       (loop)])
+    (displayln "connection lost")
+    (ws-close! c)))
 
 (define stop-service
   (ws-serve #:port (string->number (vector-ref (current-command-line-arguments)
