6175d5030b507aa4cab638f25db11f2110860f7d
Kaiyang Wu <self@origincode.me>
2024-12-05 00:22:59 -0800

    fix(server): try to fix 100% cpu usage
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- server.rkt
+++ server.rkt
@@ -5,15 +5,14 @@
          net/rfc6455
          json)
 
-(define mpd-conn (mpd-connect))
-
 (define (connection-handler c state)
   (displayln "connection received")
   (let loop ([previnfo #f])
+    (define mpd-conn (mpd-connect))
     (define currinfo
       (hash 'current (mpd-currentsong mpd-conn) 'next (mpd-nextsong mpd-conn)))
-    (if (equal? previnfo currinfo)
-        (loop currinfo)
+    (mpd-close-connection mpd-conn)
+    (when (not (equal? previnfo currinfo))
         (ws-send! c (jsexpr->bytes currinfo)))
     (sleep 5)
     (loop currinfo)))
@@ -25,4 +24,3 @@
 (printf "Server running. Hit enter to stop service.\n")
 (void (read-line))
 (stop-service)
-(mpd-close-connection mpd-conn)
