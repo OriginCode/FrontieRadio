74bd89815a2b92c47d3dbc1dd1b8bd15f3db895c
Kaiyang Wu <self@origincode.me>
2024-12-05 03:17:11 -0800

    feat(web): add audio play button, use flex to align items (I hate CSS)
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,54 @@
 import './App.css';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import useWebSocket from "react-use-websocket";
 
 function App() {
-    const WS_URL = "ws://127.0.0.1:8081/";
+    const AUDIO_URL = "https://radio-raw.origincode.me/";
+    const WS_URL = "wss://radio-api.origincode.me/";
+
+    const [audio] = useState(new Audio(AUDIO_URL));
+    const [playing, setPlaying] = useState(false);
+    const playOrPause = () => setPlaying(!playing);
+    useEffect(() => {
+            playing ? audio.play() : audio.pause();
+        },
+        [audio, playing]
+    );
+    useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+            audio.removeEventListener('ended', () => setPlaying(false));
+        };
+    }, [audio]);
+
     const {lastMessage} = useWebSocket(WS_URL);
     let lastMessageData = lastMessage ? JSON.parse(lastMessage.data) : null;
+
     return (
         <div className="App">
-            <table>
-                <tbody>
-                <tr align="left">
-                    <th scope="row" align="right"><strong>CURRENT PLAYING:&nbsp;</strong></th>
+            <div className="flex-item">
+                <div className="header">
+                    <strong>CURRENT PLAYING:&nbsp;</strong>
+                </div>
+                <div className="content">
                     {lastMessageData ? lastMessageData.current.Artist + " - " + lastMessageData.current.Title : "No song playing"}
-                </tr>
-                <tr align="left">
-                    <th scope="row" align="right"><strong>NEXT SONG:&nbsp;</strong></th>
+                </div>
+            </div>
+            <div className="flex-item">
+                <div className="header">
+                    <strong>NEXT SONG:&nbsp;</strong>
+                </div>
+                <div className="content">
                     {lastMessageData ?
                         (Object.keys(lastMessageData.next).length !== 0 ?
                             lastMessageData.next.Artist + " - " + lastMessageData.next.Title
                             : "No next song")
                         : "No next song"}
-                </tr>
-                </tbody>
-            </table>
+                </div>
+            </div>
+            <div className="flex-item">
+                <button onClick={playOrPause}>{playing ? "Pause" : "Play"}</button>
+            </div>
         </div>
     );
 }
